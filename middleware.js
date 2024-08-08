f301bc75072db8f5cf8a92bf1fdeec78f41360d1
David LeVack <74128824+davidlevack@users.noreply.github.com>
2024-08-07 22:25:48 -0400

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,11 +4,12 @@ export default function middleware(req) {
   return next({
     headers: {
       'Referrer-Policy': 'origin-when-cross-origin',
-      'X-Frame-Options': 'DENY',
+      'X-Frame-Options': 'ALLOW-FROM *', // Changed from 'DENY' to allow iframes
       'X-Content-Type-Options': 'nosniff',
       'X-DNS-Prefetch-Control': 'on',
       'Strict-Transport-Security':
         'max-age=31536000; includeSubDomains; preload',
+      'Content-Security-Policy': "frame-ancestors 'self' *;", // Added to allow iframe embedding
     },
   });
 }
